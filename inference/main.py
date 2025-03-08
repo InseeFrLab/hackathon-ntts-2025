4206fdfada2990b5fb87b84262ed3861dd4e9593
rayaberova <raya.berova@insee.fr>
2025-03-08 21:25:38 +0000

    test argo predict api

--- inference/main.py
+++ inference/main.py
@@ -1,11 +1,11 @@
-import requests
-import s3fs
-import os
-import json
-import geopandas as gpd
 import argparse
+import json
+import os
 import time
 
+import geopandas as gpd
+import requests
+import s3fs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Raster tiling pipeline")
@@ -19,29 +19,32 @@ if __name__ == "__main__":
     start_time = time.time()
 
     fs = s3fs.S3FileSystem(
-        client_kwargs={'endpoint_url': 'https://'+'minio.lab.sspcloud.fr'},
-        key=os.environ["AWS_ACCESS_KEY_ID"], 
-        secret=os.environ["AWS_SECRET_ACCESS_KEY"]
-        )
+        client_kwargs={"endpoint_url": "https://" + "minio.lab.sspcloud.fr"},
+        key=os.environ["AWS_ACCESS_KEY_ID"],
+        secret=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
 
     url = "https://hackathon-ntts-2025.lab.sspcloud.fr/predict_nuts"
-    response = requests.get(url, params={'nuts_id': nuts3, 'year': year})
+    response = requests.get(url, params={"nuts_id": nuts3, "year": year})
 
-    geojson_string = response.json()["predictions"]
-    geojson_obj = json.loads(geojson_string)
+    if response.status_code == 200:
+        geojson_string = response.json()["predictions"]
+        geojson_obj = json.loads(geojson_string)
 
-    data = []
-    for feature in geojson_obj["features"]:
-        feature_id = feature["id"]
-        label = feature["properties"]["label"]
-        coordinates = feature["geometry"]["coordinates"]
+        data = []
+        for feature in geojson_obj["features"]:
+            feature_id = feature["id"]
+            label = feature["properties"]["label"]
+            coordinates = feature["geometry"]["coordinates"]
 
-        data.append({"id": feature_id, "label": label, "coordinates": coordinates})
-    df = gpd.GeoDataFrame(data)
+            data.append({"id": feature_id, "label": label, "coordinates": coordinates})
+        df = gpd.GeoDataFrame(data)
 
-    filepath_out = f"s3://projet-hackathon-ntts-2025/data-predictions/CLCplus-Backbone/SENTINEL2/{year}/250/"
-    with fs.open(filepath_out, 'wb') as f:
-        df.to_file(f, driver="GPKG", index=False)
+        filepath_out = f"s3://projet-hackathon-ntts-2025/data-predictions/CLCplus-Backbone/SENTINEL2/{year}/250/predictions_{nuts3}.gpkg"
+        with fs.open(filepath_out, "wb") as f:
+            df.to_file(f, driver="GPKG", index=False)
 
-    end_time = time.time() - start_time
-    print(f"{nuts3} predicted in {round(end_time/60)} min and registered here {filepath_out}")
+        end_time = time.time() - start_time
+        print(f"{nuts3} predicted in {round(end_time/60)} min and registered here {filepath_out}")
+    else:
+        print(f"API error {response.status_code}")
