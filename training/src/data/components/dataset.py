d10a050007db78b6d69601860c19a928ef0cbe59
Thomas Faria <thomasfaria31@gmail.com>
2025-03-09 18:42:38 +0000

    [fix] deals with unspecified labels

--- training/src/data/components/dataset.py
+++ training/src/data/components/dataset.py
@@ -67,6 +67,8 @@ class SegmentationDataset(Dataset):
             )
 
             label = np.load(self.labels[idx])
+            # Temp
+            label[(label > 12) | (label < 0)] = 0 # Replace labels >12 or <0 with 0
 
         # Transforms
         sample = self.transform(image=np.transpose(si.array, [1, 2, 0]), mask=label)
